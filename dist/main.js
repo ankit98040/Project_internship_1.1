d2c2a989a757a2d6f2d58c378906750bdeeed3c1
Ankit Pramanik <59945244+ankit98040@users.noreply.github.com>
2020-04-18 09:37:57 +0530

    Add files via upload
    the doc1.html contains the main html file and the scr folder contains the es6 file which is converted to es5 using babel and stored in the Dist file. all the codes are provided .

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function add_google_logo() {
+    var src = "https://dog.ceo/api/breeds/image/random";
+    show_image("https://dog.ceo/api/breeds/image/random", 276, 110, "Google Logo");
+}
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    document.body.appendChild(img);
+}
